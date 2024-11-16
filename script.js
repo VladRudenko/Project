d4947263da981b41636b210737b459d1d184a26d
VladRudenko <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~>
2024-11-16 02:54:57 +0200

    Was changed logic html files (instead of 3 files data were added to 1 index.html file); was added script.js phile for kreation new html logic that was described before

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
-// const refs = {
+const refs = {
+    navigation: document.querySelector("#tabs-content-1 [data-navigation]"),//єдинийй екземпляр, котрий знаходиться в першу чергу по ID
+    content: document.querySelector("#tabs-content-1 [data-main-content]")
+}; 
+
+refs.navigation.addEventListener("click", onChangeNavigation);
+
+function onChangeNavigation(event) {
+    const {target} = event;
+
+    if (target.nodeName !== "BUTTON") return;
+
+    const currentButton = target; //класс в окремій змінній
+    const prevActiveButton = refs.navigation.querySelector(".btnActive"); //шукаэмо конкретно в div тег data-navigation
+    const prevActiveContent = refs.content.querySelector(".contentShow");
+
+    // console.log("prevActiveButton", prevActiveButton);
+
+    if (prevActiveButton) {
+        prevActiveButton.classList.remove("btnActive");
+        prevActiveContent.classList.remove("contentShow");
+    }
+
+    const currentTab = currentButton.dataset.tab;
+
+    const currentContent = refs.content.querySelector(`[data-content="${currentTab}"]`);
+        
+    console.log(currentContent);
     
-// }
+    currentButton.classList.add("btnActive");
+    currentContent.classList.add("contentShow");
+}
